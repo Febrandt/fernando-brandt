1a6301f228bb789d29c512631902fad99110bb24
Febrandt <fernandombrandt@gmail.com>
2025-04-24 21:35:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import InitialHero from './components/InitialHero'
+
+
+function App() {
+
+  return (
+    <>
+      <InitialHero></InitialHero>
+      <div className='bg-light mx-10 sm:mx-20 p-10 rounded-lg shadow-lg mt-10 text-center'>
+        <em className='text-2xl tracking-[0.09rem] text-center'>Brief</em>
+        <p className='mt-2 text-justify'>
+          Sou desenvolvedor especializado em criação de sistemas web sob medida e websites modernos. Faço desde a lógica de sistemas robustos até a interface que o usuário vê e interage. Meu foco é entregar soluções que funcionam de verdade — rápidas, seguras e com visual profissional.
+        </p>
+      </div>
+      <div className='bg-light mx-10 sm:mx-20 p-10 rounded-lg shadow-lg mt-10 flex flex-col'>
+        <em className='text-2xl tracking-[0.09rem] text-center'>Formação</em>
+        <div className='flex flex-row gap-5 mt-5'>
+          <img src="/src/assets/senai.png" className='w-20 h-20 rounded-full'></img>
+          <div className='flex flex-col'>
+            <em>Curso técnico em desenvolvimento de sistemas</em>
+
+            <p className='mt-2'>
+              Adquiri conhecimentos sólidos em programação, banco de dados, e desenvolvimento de aplicações web.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='bg-light mx-10 sm:mx-20 p-10 rounded-lg shadow-lg mt-10 text-center'>
+        <em className='text-2xl tracking-[0.09rem] text-center'>Serviços</em>
+        <p className='mt-1 text-justify'>
+          O que eu posso desevolver para você?
+        </p>
+        <ul className='list-disc ml-5 mt-4 text-left'>
+          <li style={{ fontWeight: 600 }}>Sistemas de gestão sob medida (clientes, estoque, chamados, etc.)</li>
+          <li style={{ fontWeight: 600 }}>Sites responsivos e otimizados para Google</li>
+          <li style={{ fontWeight: 600 }}>Integração entre plataformas (pagamento, CRM, etc.)</li>
+          <li style={{ fontWeight: 600 }}>Painéis administrativos.</li>
+        </ul>
+      </div>
+      <footer className='bg-dark text-light p-5 shadow-lg mt-10 text-center'>
+        <p className='text-sm !text-white'>
+          Entre em contato: <a href="mailto:seuemail@example.com" className=' !text-blue-400' style={{ fontFamily: 'Liden Hill' }}>fernandombrandt@gmail.com</a> | Telefone ou Whatssap: <a href="tel:+55051994285159" className='!text-blue-400 '>(51) 99428-5159</a>
+        </p>
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/InitialHero.jsx
@@ -0,0 +1,15 @@
+function InitialHero() {
+    return <>
+        <div className='flex flex-row items-center sm:h-[80vh] lg:h-[80vh] md:h-[80vh] h-[75vh] px-16 flex-wrap-reverse align-middle'>
+            <div className='flex-2/3 sm:mt-0 -mt-40'>
+                <h1 className='sm:text-7xl text-[12vw]/[12vw] line mb-2'>Fernando Brandt</h1>
+                <em className='text-2xl tracking-[0.09rem]'>Programador de Sistemas e Websites</em>
+            </div>
+            <div className='flex justify-center flex-1/3' >
+                <img src='/src/assets/profile.webp' className='w-[40vw] sm:w-[15rem] rounded-full'></img>
+            </div>
+        </div>
+    </>
+}
+
+export default InitialHero
